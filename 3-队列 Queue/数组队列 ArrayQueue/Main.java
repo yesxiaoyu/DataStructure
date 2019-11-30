b20c0fcd0f3c76ac929821525a500844ff55a4ff
yesxiaoyu <1757732597@qq.com>
2019-11-30 10:43:57 +0800

    2019-11-30

---
+++ 3-队列 Queue/数组队列 ArrayQueue/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for(int i = 0 ; i < 10 ; i ++){
+            queue.enqueue(i);
+            System.out.println(queue);
+            if(i % 3 == 2){
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
